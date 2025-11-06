e020f9cd5b007b4e7baef1516c0c7d67bbf2dbce
reid-sutherland <reid.sutherland96@gmail.com>
2025-11-05 19:24:35 -0700

    EXILED v9.10.1

--- VendingMachine/MainPlugin.cs
+++ VendingMachine/MainPlugin.cs
@@ -23,7 +23,7 @@ public class MainPlugin : Plugin<Config>
 
     public override Version Version { get; } = new(1, 1, 0);
 
-    public override Version RequiredExiledVersion { get; } = new(9, 6, 1);
+    public override Version RequiredExiledVersion { get; } = new(9, 10, 1);
 
     public override PluginPriority Priority { get; } = PluginPriority.Low;
 
